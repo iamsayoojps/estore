23e0c9c8c7daee7b4fe0226d44560c1f53e69392
sayooj <sayoojps57@gmail.com>
2025-04-22 12:39:03 +0530

    last commit

--- src/Components/CatNav/index.js
+++ src/Components/CatNav/index.js
@@ -1,15 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCategories } from '../../Redux/Category/actions';
 import './_cat-nav.scss';
 
 const CatNav = ()=>{
+    const categories = useSelector(state=>state.categoryReducer.categories);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getCategories());
+    },[]);
 
     return(
         <>
             <div className='cat-nav-container container'>
                 <ul>
-                    <li className='list-items'> <a href='#'> Men </a> </li>
-                    <li className='list-items'> <a href='#'> Women </a> </li>
-                    <li className='list-items'> <a href='#'> Kids </a> </li>
-                    <li className='list-items'> <a href='#'> Best Offers </a> </li>
+                    <li className='list-items'>
+                        <Link to="/"> Home </Link>
+                    </li>
+
+                    {
+                        categories.map((category)=>{
+                            if(category.parent_category_id === null){
+                                return(
+                                    <li className='list-items'> <a href='#'> {category.category} </a> </li>
+                                )
+                            }
+                        })
+                    }
+                    
                 </ul>
             </div>
         </>
